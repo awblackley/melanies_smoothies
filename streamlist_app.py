203286176bf8a028166774eae097b9b3036db9f1
awblackley <awblackley@yahoo.com>
2025-06-25 11:27:50 +1200

    Update streamlist_app.py
    Make changes bc of switch from SIS to SnIS

--- streamlist_app.py
+++ streamlist_app.py
@@ -1,6 +1,5 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
@@ -12,7 +11,9 @@ st.write(
 title = st.text_input("Name on smoothie:")
 st.write("The name on your smoothie will be:", title)
 
-session = get_active_session()
+cnx=st.connection('snowflake')
+session=cnx.session
+
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('fruit_name'))
 ## st.dataframe(data=my_dataframe, use_container_width=True)
 ## if "selected" in st.session_state:
